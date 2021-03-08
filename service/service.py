67adb6a9d16a7d779c3079d8dfd4bb441b93d13b
Pegah Natanzi <pegahnatanzi@Pegahs-MacBook-Pro.local>
2021-03-08 15:20:08 -0500

    added skeleton files

---
+++ service/service.py
@@ -0,0 +1,44 @@
+"""
+Customers Service
+
+This service will create, add, update, list, read, and delete customer profiles.
+"""
+
+import os
+import sys
+import logging
+from flask import Flask, jsonify, request, url_for, make_response, abort
+from flask_api import status  # HTTP Status Codes
+
+# For this example we'll use SQLAlchemy, a popular ORM that supports a
+# variety of backends including SQLite, MySQL, and PostgreSQL
+from flask_sqlalchemy import SQLAlchemy
+from service.models import YourResourceModel, DataValidationError
+
+# Import Flask application
+from . import app
+
+######################################################################
+# GET INDEX
+######################################################################
+@app.route("/")
+def index():
+    """ Root URL response """
+    return (
+        jsonify(
+            name="Customers Demo REST API Service",
+            version="1.0",
+            paths=url_for("list_customers", _external=True),
+        status.HTTP_200_OK,
+    )
+
+
+######################################################################
+#  U T I L I T Y   F U N C T I O N S
+######################################################################
+
+
+def init_db():
+    """ Initialies the SQLAlchemy app """
+    global app
+    YourResourceModel.init_db(app)
