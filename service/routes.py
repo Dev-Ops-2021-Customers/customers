31e359fa908dd18811b30881dc8a5a880148528f
Pegah Natanzi <pegahnatanzi@Pegahs-MacBook-Pro.local>
2021-03-11 18:33:34 -0500

    Changed service back to routes

--- service/routes.py
+++ service/routes.py
@@ -28,8 +28,8 @@ def index():
         jsonify(
             name="Customers Demo REST API Service",
             version="1.0",
-            paths=url_for("list_customers", _external=True),
-        status.HTTP_200_OK,
+        ),
+        status.HTTP_200_OK
     )
 
 
